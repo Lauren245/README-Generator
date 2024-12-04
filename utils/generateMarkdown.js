fa933147981925466d03156132b52fda40b382c1
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2024-12-04 13:17:14 -0800

    added specific cases for no license in the switch statements that handle badge images and license URLs. Added an error message to the default cases. Removed an unnecessary check from the if statement in the renderLicenseSection function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,13 +23,16 @@ function renderLicenseBadge(license) {
      badgeImg = "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
       break;
     }
-    //no license falls here. I'm not sure if I should make it a seperate case. It is a selectable option, but it should also set badgeImg to empty string.
-    default: {
+    case "no license": {
       badgeImg = "";
       break;
     }
+    default: {
+      badgeImg = "";
+      console.error("An error has occured while assigning a badge image.");
+      break;
+    }
   }
-
   return badgeImg;
 }
 
@@ -58,22 +61,23 @@ function renderLicenseLink(license) {
       licenseLink = "https://www.boost.org/LICENSE_1_0.txt";
       break;
     }
-    //no license falls here. I'm not sure if I should make it a seperate case. It is a selectable option, but it should also set licenseLink to empty string.
-    default: {
+    case "no license": {
       licenseLink = "";
       break;
     }
+    default: {
+      licenseLink = "";
+      console.error("An error occured while assigning a license link.")
+      break;
+    }
   }
-
   return licenseLink;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  //TODO: expand this
-  //It should be impossible for license to be null, but I wanted to account for edge cases.
-  if((license === "no license") || (license === null)){
+  if((license === "no license")){
     return "";
   }
   return `Licensed under the ${license} license.`;
